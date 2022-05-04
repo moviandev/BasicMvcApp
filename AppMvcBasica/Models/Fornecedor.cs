6830045465765d827d26da5df37150300aec4bd5
Matheus Viana <matheus_o_viana@hotmail.com>
2022-05-03 23:30:06 -0300

    second commit

---
+++ AppMvcBasica/Models/Fornecedor.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppMvcBasica.Models
+{
+    public class Fornecedor : Entity
+	{
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        public TipoForncedor TipoForncedor { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+         public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
+
---
+++ AppMvcBasica/Models/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppMvcBasica.Models
+{
+	public class Produto : Entity
+	{
+		public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
+        public string Descricao { get; set; }
+
+        public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        public decimal Valor { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public virtual Fornecedor Fornecedor { get; set; }
+    }
+}
+
